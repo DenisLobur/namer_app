01ffe8995b6db2a90c4e7a777df3109d64eded4c
DenysLobur <denismachtalles@gmail.com>
2023-08-10 18:52:23 +0300

    Complete!

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
 import 'package:provider/provider.dart';
 
 void main() {
@@ -63,7 +62,7 @@ class _MyHomePageState extends State<MyHomePage> {
         page = GeneratorPage();
         break;
       case 1:
-        page = Placeholder();
+        page = FavouritesPage();
         break;
       default:
         throw UnimplementedError('no widget for $selectedIndex');
@@ -175,6 +174,35 @@ class BigCard extends StatelessWidget {
           style: style,
         ),
       ),
+    );
+  }
+}
+
+class FavouritesPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    var appState = context.watch<MyAppState>();
+    var favourites = appState.favorites;
+
+    if (favourites.isEmpty) {
+      return Center(
+        child: Text("No favorite words yet"),
+      );
+    }
+
+    return ListView(
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(20),
+          child: Text('You have '
+              '${favourites.length} favorites:'),
+        ),
+        for (var fav in favourites)
+          ListTile(
+            leading: Icon(Icons.favorite),
+            title: Text(fav.asLowerCase),
+          ),
+      ],
     );
   }
 }
